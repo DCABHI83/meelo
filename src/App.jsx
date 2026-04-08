3d21db6765048c905b744e8bb3f0cc2a3ab0863b
DCABHI83 <araj10569@gmail.com>
2026-04-08 21:13:07 +0530

    project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from 'react'
+import {HashRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import Portfolio from './pages/Portfolio'
+import Projects from './pages/Projects'
+import About from './pages/About'
+import Header from './UI/Header'
+import Footer from './UI/Footer'
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <>
+   <HashRouter>
+
+<Routes>
+<Header/>
+
+<Route path='/' element = {<Home/>}/>
+<Route path='/contact' element = {<Contact/>}/>
+<Route path='/portfolio' element = {<Portfolio/>}/>
+<Route path='/project' element = {<Projects/>}/>
+<Route path='/about' element = {<About/>}/>
+
+<Footer/>
+
+</Routes>
+
+   </HashRouter>
+   
+   </>
   )
 }
 
---
+++ src/pages/Projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+   <>
+   <div>
+    projects
+   </div>
+   </>
+  )
+}
+
+export default Projects
