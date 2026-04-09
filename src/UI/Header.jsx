ad6b8b649695e54fe1397bd256cd8b96f3b890d0
DCABHI83 <araj10569@gmail.com>
2026-04-09 17:53:36 +0530

    finished navbar

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { CiLinkedin } from "react-icons/ci";
 const Header = () => {
+  const [isActive,setisActive] = useState("home")
   return (
-    <div>
-      Header
+    <div className='flex items-center justify-around p-5 border'>
+      <div>
+        HALO
+      </div>
+   <div className='flex gap-5 '>
+      <NavLink to={'/'} className={`${isActive === "home" ? 'text-[#A4A4DB]': '' } `} onClick={()=>setisActive("home")}>Home</NavLink>
+  <NavLink to={'about/'} className={`${isActive === "about" ? 'text-[#A4A4DB]': '' } `} onClick={()=>setisActive("about")}>About</NavLink>
+  <NavLink to={'/portfolio'} className={`${isActive === "portfolio" ? 'text-[#A4A4DB]': '' } `} onClick={()=>setisActive("portfolio")}>Portfolio</NavLink>
+  <NavLink to={'/projects'} className={`${isActive === "projects" ? 'text-[#A4A4DB]': '' } `} onClick={()=>setisActive("projects")}>Projects</NavLink>
+  <NavLink to={'/contact'} className={`${isActive === "contact" ? 'text-[#A4A4DB]': '' } `} onClick={()=>setisActive("contact")}>Contact</NavLink>
+   </div>
+   <div className='icons flex items-center  gap-4'>
+    <NavLink className='relative w-[45px] h-[45px] border rounded-[5px]'>
+       <div className='absolute w-[45px] h-[37px] border border-b-2  rounded-[5px]'></div>
+       <div className='flex justify-center items-center z-[999] pt-2.5'>  <FaGithub /></div>
+    </NavLink>
+     <NavLink className='relative w-[45px] h-[45px] border rounded-[5px]'>
+       <div className='absolute w-[45px] h-[37px] border border-b-2  rounded-[5px]'></div>
+       <div className='flex justify-center items-center z-[999] pt-2.5'><CiLinkedin /></div>
+    </NavLink>
+    <NavLink className='relative w-[45px] h-[45px] border rounded-[5px]'>
+       <div className='absolute w-[45px] h-[37px] border border-b-2  rounded-[5px]'></div>
+       <div className='flex justify-center items-center z-[999] pt-2.5'>  <FaInstagram />
+</div>
+    </NavLink>
+ 
+   </div>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import CircularText from '../components/CircularText'
-
+import { NavLink } from 'react-router-dom'
 const Home = () => {
   return (
    <>
    <div>
-    <CircularText 
-     text="I*AM*AVAILABLE*FOR*FREELANCE"
-  onHover="speedUp"
-  spinDuration={20}
-  className="custom-class"
-    />
+
+
    </div>
    </>
   )
